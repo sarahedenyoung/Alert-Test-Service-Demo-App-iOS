0ebbd83f0d0b72fd5640c16f0d29e6828ada4641
Sarah Young <sarah.young@salesforce.com>
2020-08-04 11:27:33 -0300

    Initial Commit

---
+++ Alert Service Test App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alert Service Test App
+//
+//  Created by Sarah Young on 8/4/20.
+//  Copyright © 2020 Sarah Young. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
